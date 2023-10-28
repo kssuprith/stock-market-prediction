4fbe338d0e5ec6acb0054a1db3c610a691eac292
LAPTOP-C42CD8RL\kssup <kssuprith2015@gmail.com>
2023-10-28 15:44:21 +0530

    latest changes

---
+++ constants.py
@@ -0,0 +1,7 @@
+consumer_key = "X1f8UxZmyfr1EL41zEPgs6fqm"
+consumer_secret = "PRKyfhIoRi6MQ4ubGdnqgzvIeFjIQNv2RNV7BE4u7SL3u8k5gC"
+
+access_token = "1102428566216556544-g3wC2oJjz1lwa2T1h4hn8LpePNcidA"
+access_token_secret = "m9p20S4VK2uwaciJDe2zDJiFSxmSv3fItVHUgmbXNh77N"
+
+num_of_tweets = int(300)
